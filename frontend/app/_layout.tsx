e422da937154ed8c398f7b6f6f33aba9836cc5ee
rishabh-2410 <rish.abh26@outlook.com>
2026-07-09 00:21:36 +0530

    setup app theme and styles

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useEffect } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +13,26 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const [fontsLoaded] = useFonts({
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf')
+})
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  },[fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
