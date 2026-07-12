0547599e52df69265ed9ad7ff49013775a8fa035
rishabh-2410 <rish.abh26@outlook.com>
2026-07-12 23:57:04 +0530

    add neon db

--- backend/internal/db/conn/conn.go
+++ backend/internal/db/conn/conn.go
@@ -19,23 +19,24 @@ func Connect() (*sql.DB, error) {
 	}
 
 	// Initialize string parameters
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	sslMode := os.Getenv("DB_SSLMODE")
+	// host := os.Getenv("DB_HOST")
+	// port := os.Getenv("DB_PORT")
+	// user := os.Getenv("DB_USER")
+	// password := os.Getenv("DB_PASSWORD")
+	// dbName := os.Getenv("DB_NAME")
+	// sslMode := os.Getenv("DB_SSLMODE")
 
 	// Connection string for DB (DSN: Data Source Name)
-	connectionString := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		host,
-		port,
-		user,
-		password,
-		dbName,
-		sslMode,
-	)
+	connectEnv := os.Getenv("DB_MODE")
+	var connectionString string
+	switch connectEnv {
+	case "dev":
+		connectionString = os.Getenv("DB_DEV")
+	case "prod":
+		connectionString = os.Getenv("DB_PROD")
+	default:
+		return nil, fmt.Errorf("invalid connection mode")
+	}
 
 	// Open connection to DB
 	db, err := sql.Open("postgres", connectionString)
